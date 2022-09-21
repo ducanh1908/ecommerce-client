0364768ad6c54bc9fc06dc144ff3739614c3ba79
DucAnh <ducanh.le1908@gmail.com>
2022-09-21 09:08:01 +0700

    done categories

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Categories from '../../components/Categories/Categories';
 import Navbar from '../../components/Navbar/Navbar'
 import Slider from '../../components/Slider/Slider';
 import Announcement from './../../components/Announcement/Announcement';
@@ -9,6 +10,7 @@ const Home = () => {
         <Announcement/>
        <Navbar />
        <Slider/>
+       <Categories />
     </div>
   )
 }
